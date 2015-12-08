28021086787e9056f82ef1681b48d816f557b71d
Ian Smith <iansmith1026@gmail.com>
2015-12-08 11:44:34 -0700

    created autoResponse module

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-//This is where you should incorporate your modules and execute your app
+// This is where you should incorporate your modules and execute your app
+
+var autoRespond = require('./autoResponse');
+
+console.log(autoRespond.getResponse('Wednesday'));
+console.log(autoRespond.getResponse('Saturday'));
--- autoResponse.js
+++ autoResponse.js
@@ -1,1 +1,21 @@
-//Write your Constructor Function that will generate a different response for each day of the week
+// Write your Constructor Function that will generate a different response for each
+// day of the week.
+
+exports.getResponse = function(str) {
+  switch(str) {
+    case 'Monday':
+      return 'Ugh, Mondays...';
+    case 'Tuesday':
+      return "Wow, it's Tuesday";
+    case 'Wednesday':
+      return 'Hump day!!!';
+    case 'Thursday':
+      return 'Tomorrow is Friday!';
+    case 'Friday':
+      return 'Time to get down.';
+    case 'Saturday':
+      return "Sleep in! It's Saturday!";
+    case 'Sunday':
+      return "Sleep in again! But don't forget about homework!";
+  }
+};
